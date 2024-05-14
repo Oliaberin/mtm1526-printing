5ce8d73e6f219f084f0eb469f0024a1ff3233e1c
Oliaberin <oliveraberin@hotmail.com>
2024-05-14 10:49:47 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+alert("JS commenced!");
+
+//console.log("Your program works fine!");
+
+
+//declaration of strings
+let fname = "Ken";
+//let feedback = "Hello "+fname;
+
+
+
+let feedback = `Hello ${fname}`;
+console.log(feedback);
+
+//print 8
+let num1 = 5;
+let num2 = 3;
+alert(num1 + num2);
